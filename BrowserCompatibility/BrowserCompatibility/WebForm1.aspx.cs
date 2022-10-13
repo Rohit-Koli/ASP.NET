e945dc865ec6dc84808ba0e830b418a3575415dc
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2022-10-13 08:37:02 +0530

    Browser Compatibility

---
+++ BrowserCompatibility/BrowserCompatibility/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BrowserCompatibility
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpBrowserCapabilities b;
+            b = Request.Browser;
+            Response.Write("Browser Name is :"+b.Browser);
+            Response.Write("Browser Version is :" + b.Version+"<br>");
+            Response.Write("Browser Support JavaScript is :" + b.JavaScript + "<br>");
+            Response.Write("Browser S is :" + b.Version + "<br>");
+            string url = Request.Url.AbsolutePath;
+            Response.Write("Url og curent Page is :"+url+"<br>");
+            string os = Environment.OSVersion.ToString();
+            Response.Write("Windows Version is --:"+os);
+        }
+    }
+}
